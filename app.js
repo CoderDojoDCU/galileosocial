12bbcadd0e447cb6d2c7daf5667e6b99aaa24441
imduffy15 <ian@ianduffy.ie>
2014-10-10 23:04:12 +0100

    apply source formatting

--- app.js
+++ app.js
@@ -12,97 +12,97 @@ var currentTweets = [];
 var latestId = 0;
 
 var T = new Twit({
-    consumer_key:         ''
-  , consumer_secret:      ''
-  , access_token:         ''
-  , access_token_secret:  ''
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(3000, function() {
-	console.log('Listening on port %d', server.address().port);
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
 });
 
-app.get('/tweets', function(req, res){
-  res.send(currentTweets);
+app.get('/tweets', function (req, res) {
+    res.send(currentTweets);
 });
 
 function setupBoard() {
-	if(boardAvailable) {
-		board.on("ready", function() {
-			this.pinMode(pin, this.MODES.OUTPUT);
-			queryTwitter(trend, pin);
-		});
-	} else {
-		queryTwitter(trend, pin);
-	}
+    if (boardAvailable) {
+        board.on("ready", function () {
+            this.pinMode(pin, this.MODES.OUTPUT);
+            queryTwitter(trend, pin);
+        });
+    } else {
+        queryTwitter(trend, pin);
+    }
 }
 
 function queryTwitter(trendMessage, pin) {
-	console.log("Processing trend " + trendMessage + " " +new Date());
-	if(trendMessage) {
-		T.get('search/tweets', { q: trendMessage, count: 100, since_id: latestId }, function(err, data) {
-		  if(err) {
-		  		console.log(err);
-		  		setTimeout(function () {
-					queryTwitter(trend, pin)
-				}, 5000);
-		  }
-		  else if(data.statuses && data.statuses.length > 0) {
-		  	getTweetList(data.statuses);
-		  	processTrend(currentTweets, pin);
-		  }
-		});
-	} 
+    console.log("Processing trend " + trendMessage + " " + new Date());
+    if (trendMessage) {
+        T.get('search/tweets', { q: trendMessage, count: 100, since_id: latestId }, function (err, data) {
+            if (err) {
+                console.log(err);
+                setTimeout(function () {
+                    queryTwitter(trend, pin)
+                }, 5000);
+            }
+            else if (data.statuses && data.statuses.length > 0) {
+                getTweetList(data.statuses);
+                processTrend(currentTweets, pin);
+            }
+        });
+    }
 }
 
 function processTweetData(tweetData) {
-	var tweet = {};
-	tweet.created = tweetData.created_at;
-	tweet.message = tweetData.text;
-	tweet.user = tweetData.user.screen_name;
-	tweet.profile = tweetData.user.profile_image_url;
-	tweet.id = parseInt(tweetData.id_str);
-	return tweet;
+    var tweet = {};
+    tweet.created = tweetData.created_at;
+    tweet.message = tweetData.text;
+    tweet.user = tweetData.user.screen_name;
+    tweet.profile = tweetData.user.profile_image_url;
+    tweet.id = parseInt(tweetData.id_str);
+    return tweet;
 }
 
 function getTweetList(tweetList) {
-	currentTweets = [];
-	for(var i = 0; i < tweetList.length; i++) {
-		var tweet = processTweetData(tweetList[i]);
-		currentTweets.push(tweet);
-		setHighestId(tweet.id);
-	}
+    currentTweets = [];
+    for (var i = 0; i < tweetList.length; i++) {
+        var tweet = processTweetData(tweetList[i]);
+        currentTweets.push(tweet);
+        setHighestId(tweet.id);
+    }
 }
 
 function processTrend(tweeters, pin) {
-	if(tweeters.length > 1) {	
-		executePin(pin);
-		console.log(tweeters);
-	}
-	setTimeout(function () {
-		queryTwitter(trend, pin)
-	}, 50000);
+    if (tweeters.length > 1) {
+        executePin(pin);
+        console.log(tweeters);
+    }
+    setTimeout(function () {
+        queryTwitter(trend, pin)
+    }, 50000);
 }
 
 function executePin(pin) {
-	if(boardAvailable) {
-	  	setTimeout(flash(pin, 0, 0), 100);
-	}
+    if (boardAvailable) {
+        setTimeout(flash(pin, 0, 0), 100);
+    }
 }
 
 function flash(pin, increment, value) {
-	board.digitalWrite(pin, 1);
-	if(increment > 50)
-		setTimeout(function () {
-			flash(pin, increment++, (value ^= 1));
-		}, 200);
+    board.digitalWrite(pin, 1);
+    if (increment > 50)
+        setTimeout(function () {
+            flash(pin, increment++, (value ^= 1));
+        }, 200);
 }
 
 function setHighestId(currentId) {
-	if(currentId > latestId) 
-		latestId = currentId;
+    if (currentId > latestId)
+        latestId = currentId;
 }
 
 setupBoard();
